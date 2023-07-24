d1da1f3891ec8de42f1c626e973037698c05d0ef
Prabakaran-M <mspk028@gmail.com>
2023-07-24 18:35:45 +0530

    bug fixed

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -104,7 +104,7 @@ const Preferences = () => {
             onChange={(e) => setNoOfDays(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             min={0}
-            placeholder="Phone Number"
+            placeholder="No Of Days"
             required
           />
         </div>
